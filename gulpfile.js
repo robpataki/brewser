63aff7f0bc0910441d5e63d10326ace03dffe87c
Robert Pataki <rob@handcrafteddigital.london>
2015-08-06 20:03:28 +0100

    Set up versioning
    - Add version.json to the project root
    - Make Gulp update package.json, bower.json, brewser.js and
      brewser.min.js when running 'gulp release' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,18 @@
 // grab our packages
-var gulp        = require('gulp'),
-    del         = require('del'),
-    jshint      = require('gulp-jshint'),
-    uglify      = require('gulp-uglify'),
-    rename      = require('gulp-rename'),
-    replace     = require('gulp-replace'),
-    config      = require('./package.json');
+var gulp            = require('gulp'),
+    del             = require('del'),
+    jsonTransform   = require('gulp-json-transform'),
+    jshint          = require('gulp-jshint'),
+    uglify          = require('gulp-uglify'),
+    rename          = require('gulp-rename'),
+    replace         = require('gulp-replace'),
+    versionConfig   = require('./version.json'),
+    npmConfig       = require('./package.json'),
+    bowerConfig     = require('./bower.json');
 
-// define the default task and add the watch task to it
-gulp.task('default', ['watch']);
-
-gulp.task('release', ['jshint', 'clean:dist', 'build', 'build:min']);
-
-// Clean the dist directory
-gulp.task('clean:dist', function(cb) {
-    del([
-        'dist/**/*'
-    ], cb);
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+    gulp.watch('src/*.js', ['jshint']);
 });
 
 // configure the jshint task
@@ -26,23 +22,63 @@ gulp.task('jshint', function() {
             .pipe(jshint.reporter('jshint-stylish'));
 });
 
+// Clean the dist directory
+gulp.task('clean:dist', function(cb) {
+    del([
+        'dist/**/*'
+    ], cb);
+});
+
 // configure the build task
-gulp.task('build', function() {
+gulp.task('build', ['jshint', 'clean:dist'], function(cb) {
     return gulp.src('src/brewser.js')
-        .pipe(replace('{{VERSION}}', config.version))
         .pipe(gulp.dest('dist'));
 });
 
 // configure the minified build task
-gulp.task('build:min', function() {
+gulp.task('build:min', ['build'], function(cb) {
     return gulp.src('src/brewser.js')
-        .pipe(replace('{{VERSION}}', config.version))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('dist'));
 });
 
-// configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-    gulp.watch('src/*.js', ['jshint']);
+gulp.task('version:build', ['build:min'], function(cb) {
+    return gulp.src('dist/brewser.js')
+        .pipe(replace('{{VERSION}}', versionConfig.version))
+        .pipe(gulp.dest('dist'));
 });
+gulp.task('version:build:min', ['version:build'], function(cb) {
+    return gulp.src('dist/brewser.min.js')
+        .pipe(replace('{{VERSION}}', versionConfig.version))
+        .pipe(gulp.dest('dist'));
+});
+
+// Update the version number in the bopackagewer.json file using
+// 'version.json'
+gulp.task('version:npm', ['version:build:min'], function(cb) {
+    var contents = npmConfig;
+    contents.version = versionConfig.version;
+
+    return gulp.src('./package.json')
+    .pipe(jsonTransform(function(data) {
+        return contents;
+    }, 4))
+    .pipe(gulp.dest('./'));
+});
+
+// Update the version number in the bower.json file using 'version.json'
+gulp.task('version:bower', ['version:npm'], function(cb) {
+    var contents = bowerConfig;
+    contents.version = versionConfig.version;
+
+    return gulp.src('./bower.json')
+    .pipe(jsonTransform(function(data) {
+        return contents;
+    }, 4))
+    .pipe(gulp.dest('./'));
+});
+
+// Define custom tasks
+gulp.task('default', ['watch']);
+gulp.task('release', ['version:bower']);
