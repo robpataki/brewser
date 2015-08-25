11785b2658fa7c9b6a33143d22a17e86fe2b4fe9
Robert Pataki <rob@handcrafteddigital.london>
2015-08-25 19:46:02 +0100

    Update README file footer

--- dist/brewser.js
+++ dist/brewser.js
@@ -10,7 +10,7 @@
         _init: function() {
 
             // Version bumped by Gulp, don't touch
-            this.VERSION = '0.2.0';
+            this.VERSION = '0.2.1';
 
             var _this = this;
 
