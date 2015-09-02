9c50c9fc0d41667e1392c3b3eda9593ef91ae6f2
Robert Pataki <rob@handcrafteddigital.london>
2015-09-02 14:01:02 +0100

    Demo styling updates

--- dist/brewser.js
+++ dist/brewser.js
@@ -10,7 +10,7 @@
         _init: function() {
 
             // Version bumped by Gulp, don't touch
-            this.VERSION = '0.3.3';
+            this.VERSION = '0.3.6';
 
             var _this = this;
 
